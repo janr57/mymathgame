469bdddb797debb6107e5bbf8671253d4aae310b
janr <janr.devel@gmail.com>
2022-07-09 20:47:35 +0200

    Added minimal mymathgame.c

---
+++ src/mymathgame.c
@@ -0,0 +1,15 @@
+/*
+ * mymathgame.c
+ * Get a certain number by doing any + - * / operation on some others.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	return 0;
+}
+
