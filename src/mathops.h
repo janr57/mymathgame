72bcbb795b4b4062000a2774e97cacf89ec566d5
janr <janr.devel@gmail.com>
2022-07-17 20:32:03 +0200

    Corrected 'mathops.h' for rational arithmetics

--- src/mathops.h
+++ src/mathops.h
@@ -20,6 +20,12 @@ typedef struct _Mathgame {
 
 int try_mathops(mathgame_t *mg);
 void print_nums(long *nums, size_t nums_len);
+void rat_init(rational_t *r, long n, long d);
+void rat_add(rational_t *z, rational_t x, rational_t y);
+void rat_sub(rational_t *z, rational_t x, rational_t y);
+void rat_mul(rational_t *z, rational_t x, rational_t y);
+void rat_div(rational_t *z, rational_t x, rational_t y);
+void rat_reduce(rational_t *z);
 
 #endif /* MATHOPS_H___ */
 
